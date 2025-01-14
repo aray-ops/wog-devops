caffeb3a43c1bd4f9bd5da85fc95a8a134ec4005
aray-ops <almogjigga@gmail.com>
2025-01-14 15:41:10 +0200

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Constants
-SCORES_FILE_NAME = "Scores.txt"
-BAD_RETURN_CODE = -1
-
-def screen_cleaner():
-    """Clear the screen"""
-    os.system('cls' if os.name == 'nt' else 'clear')
